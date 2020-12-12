52bc66fb5d9630dc3e7a0ef7618960cb77a63560
Akshit Garg <garg.akshit@gmail.com>
2020-12-12 17:14:53 +0530

    [Desktop] Initial commit

---
+++ electron.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      webviewTag: true,
+    },
+    show: false,
+  });
+
+  win.menuBarVisible = false;
+  win.loadFile("app/index.html");
+
+  win.show();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
